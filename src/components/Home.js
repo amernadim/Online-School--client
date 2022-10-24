89f66d2fad91ef9b5eee54839234c88bbc354c9b
Your Name <you@example.com>
2022-10-24 23:25:34 +0600

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import toast from 'react-hot-toast';
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDy5upTVpgyXSjwFRrL0ZBcZ7XZrEdsQw4",
+  authDomain: "assignment-10-5d80b.firebaseapp.com",
+  projectId: "assignment-10-5d80b",
+  storageBucket: "assignment-10-5d80b.appspot.com",
+  messagingSenderId: "1031024422713",
+  appId: "1:1031024422713:web:11f098a3fdf398583a82e8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
