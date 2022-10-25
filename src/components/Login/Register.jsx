ee4cf28954bd408573bcdcdcf37379ea01ceffbd
Your Name <you@example.com>
2022-10-25 15:17:24 +0600

    added photo url and full name in register

--- src/components/Login/Register.jsx
+++ src/components/Login/Register.jsx
@@ -11,22 +11,29 @@ const Register = () => {
 	<form novalidate="" action="" className="space-y-12 ng-untouched ng-pristine ng-valid">
 		<div className="space-y-4">
 			<div>
-				<label for="email" className="block mb-2 text-sm">Email address</label>
-				<input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" />
+				<label for="name" className="block mb-2 text-sm font-semibold">Full Name</label>
+				<input type="name" name="name" id="name" placeholder="Enter Your Full Name" className="w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100" required/>
+			</div>
+			<div>
+				<label for="email" className="block mb-2 text-sm font-semibold">Photo Url</label>
+				<input type="text" name="photoURL" id="photoURL" placeholder="Enter Your Photo Url" className="w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100" required/>
+			</div>
+			<div>
+				<label for="email" className="block mb-2 text-sm font-semibold">Email address</label>
+				<input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100" required/>
 			</div>
 			<div>
 				<div className="flex justify-between mb-2">
-					<label for="password" className="text-sm">Password</label>
-					
+					<label for="password" className="text-sm font-semibold">Password</label>					
 				</div>
-				<input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" />
+				<input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100" required/>
 			</div>
 		</div>
 		<div className="">	
         <button type="button" className="btn btn-wide w-full px-8 py-3 font-semibold rounded-md  ">Register</button>
 		</div>
 			<p className="px-6 text-sm text-center ">Already Have an Account?
-				<Link rel="noopener noreferrer" to="/login" className="hover:underline text-white"> Log in</Link>.
+				<Link rel="noopener noreferrer" to="/login" className="hover:underline text-white"> Log in .</Link>
 			</p>
 	</form>
 </div>
