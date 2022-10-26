ced0e7cb65312ddec21be55190f117c7f7d08119
Your Name <you@example.com>
2022-10-26 11:37:25 +0600

    Added readme file

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Online School
+
+- ## live link 
+
+## In this project i use 
+
+- Firebase Authentication
+- Email password , Google , Github Sign in
+
+- Backend Node/express Api (Deploy vercel)
+
+- React Router Dom
+- Tailwind Css (Daisy ui)
+- Hero icon
+- React Hot Toast
+- React To Pdf
+   
+  
+
--- src/components/Courses/CourseDetails.jsx
+++ src/components/Courses/CourseDetails.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { Link, useLoaderData } from "react-router-dom";
-import { PrinterIcon } from '@heroicons/react/24/solid';
+import { ArrowDownCircleIcon } from '@heroicons/react/24/solid';
+import ReactDOM from "react-dom";
+import ReactToPdf from "react-to-pdf";
 
 
 
 
 const CourseDetails = () => {
   const course = useLoaderData();
+
+  const ref = React.createRef();
   
   // console.log(course);
   const { id, title, img, fee, desciption, video } = course;
   return (
-    <div className="w-10/12 mx-auto mt-[95px]">
+    <div className=" mx-auto mt-[95px] " ref={ref}>
       <div>
       <h2 className="text-3xl font-semibold text-center">
         Course Name : {title}
       </h2>
       <figure className="px-10 pt-10">
-        <img src={img} alt="Course" className="rounded-xl mx-auto" />
+        <img src={img} alt="Course" className="rounded-xl mx-auto " />
       </figure>
       <div className="card-body items-center text-center">
         <p className="text-xl">{desciption}</p>
@@ -25,8 +29,14 @@ const CourseDetails = () => {
           <p className="text-xl font-bold mr-5">Course Fee : {fee} Tk</p>
           <p className="text-xl font-bold">Videos : {video} </p>
         </div>
-        <div className="card-actions">
-          <button className="btn btn-md bg-blue-600 mt-3">Download <PrinterIcon className="ml-3 h-6 w-6 text-white"/></button>
+        <div className="card-actions mx-auto">
+
+          <ReactToPdf targetRef={ref} filename="div-blue.pdf">
+        {({toPdf}) => (
+            <button onClick={toPdf} className="btn btn-md bg-blue-600 mt-3">Download <ArrowDownCircleIcon className="ml-3 h-6 w-6 text-white"/></button>
+
+        )}
+       </ReactToPdf>
 
           <Link to={`/cheakout/${id}`}>
           <button className="btn btn-md bg-blue-400 text-black mt-3">Get Premium Access</button>
@@ -39,5 +49,6 @@ const CourseDetails = () => {
 };
 
 export default CourseDetails;
+
 
 
